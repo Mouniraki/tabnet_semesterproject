203a2755267c60bfc89db47df83c21e862fe207a
Mounir Raki <mouniraki@protonmail.com>
2022-05-09 00:24:10 +0200

    Change name of PATH variable to avoid conflicts with system PATH variable

--- exp/tabnet_scripts/tabnet_ieeecis_traineval_clean_n_shared.sh
+++ exp/tabnet_scripts/tabnet_ieeecis_traineval_clean_n_shared.sh
@@ -1,10 +1,10 @@
-PATH = ../models/tabnet/tabnet_n_shared
-mkdir $PATH
+PATH_TO_MODELS="../models/tabnet/tabnet_n_shared"
+mkdir $PATH_TO_MODELS
 for i in `seq 1 3`
 do
     # Name of saved model = <<dataset_name>>_<<n_steps_val>>_<<n_shared_val>>_<<n_ind_val>>-<<n_a_val>>_<<n_d_val>>_<<eps_val>>_<<n_attacks_val>>.pt
     echo "Training model with n_shared = $i"
-    python3 train.py --dataset ieeecis --model tabnet_ieeecis --model_path $PATH/ieeecis_4_$i_2_16_16_0_0.pt --n_shared $i --epochs 1000 --attack_iters 0
+    python3 train.py --dataset ieeecis --model tabnet_ieeecis --model_path $PATH_TO_MODELS/ieeecis_4_$i_2_16_16_0_0.pt --n_shared $i --epochs 1000 --attack_iters 0
     echo "Evaluating model"
-    python3 eval.py --dataset ieeecis --model tabnet_ieeecis --model_path $PATH/ieeecis_4_$i_2_16_16_0_0.pt --n_shared $i --utility_type average-attack-cost
+    python3 eval.py --dataset ieeecis --model tabnet_ieeecis --model_path $PATH_TO_MODELS/ieeecis_4_$i_2_16_16_0_0.pt --n_shared $i --utility_type average-attack-cost
 done
