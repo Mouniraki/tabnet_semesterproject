fcbbbfb5e39712b36268a4571f25eceeb71ae036
Mounir Raki <mouniraki@protonmail.com>
2022-05-17 10:44:28 +0200

    Add files for week12

---
+++ exp/tabnet_scripts/tabnet_ieeecis_eval_clean.sh
@@ -0,0 +1,22 @@
+UTILITY_TYPES="maximum satisficing cost-restrictred average-attack-cost success_rate"
+PATH_TO_MODELS="../models/tabnet/"
+mkdir $PATH_TO_MODELS
+
+echo "Model format: [n_steps|n_shared|n_ind]"
+echo "maximum,satisficing,cost-restricted,average-attack-cost,success_rate"
+for n_steps in `seq 2 8`
+do
+    for n_shared in `seq 1 3`
+    do
+        for n_ind in `seq 1 3`
+        do
+            # Name of saved model = <<dataset_name>>_<<n_steps_val>>_<<n_shared_val>>_<<n_ind_val>>-<<n_a_val>>_<<n_d_val>>_<<eps_val>>_<<n_attacks_val>>.pt
+            echo "[${n_steps}|${n_shared}|${n_ind}]"
+            for ut in $UTILITY_TYPES
+            do
+                python3 eval.py --dataset ieeecis --model tabnet_ieeecis --model_path $PATH_TO_MODELS/ieeecis_${n_steps}_${n_shared}_${n_ind}_16_16_0_0.pt --n_steps $n_steps --n_shared $n_shared --n_ind $n_ind --utility_type $ut
+            done
+            echo "----------------------------------"
+        done
+    done
+done
---
+++ parse_and_write_eval_out_csv.py
@@ -0,0 +1,28 @@
+import re
+
+# Opens the output file
+oup = open("ieeecis_clean_eval_out.csv", mode='w')
+
+# Write the first row of the file
+oup.write("model[n_steps|n_shared|n_ind],maximum,satisficing,cost-restricted,average-attack-cost,success_rate,rob_accuracy\n")
+
+# Filters everything that isn't a number/a decimal
+regex = re.compile('[^0-9.|]')
+
+# Opens the input file, performs a filtering and writes the filtered content to a CSV-file
+with open("ieeecis_clean_eval_out.txt", 'r') as inp:
+    # Skip the first two header lines
+    next(inp)
+    next(inp)
+    for line in inp:
+        if "../" in line:
+            continue
+        elif "Rob Acc" in line:
+            oup.write(regex.sub('', line))
+        elif "---" in line:
+            oup.write('\n')
+        else:
+            oup.write(regex.sub('', line)+',')
+
+    inp.close()
+oup.close()
