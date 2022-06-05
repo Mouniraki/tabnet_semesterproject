d494e48b837b01103ff04421e23a1d8c50c8c5d5
Mounir Raki <mouniraki@protonmail.com>
2022-06-05 22:40:06 +0200

    Improved parsing script, renamed output files

--- exp/tabnet_scripts/tabnet_ieeecis_train_clean.sh
+++ exp/tabnet_scripts/tabnet_ieeecis_train_clean.sh
@@ -2,6 +2,7 @@ PATH_TO_MODELS="../models/tabnet/"
 mkdir $PATH_TO_MODELS
 
 DELAY_VAL=10
+DELAY_THRESHOLD=100
 EPS_VAL=0.0
 ATTACK_ITERS=0
 
@@ -13,7 +14,7 @@ do
         do
             # Name of saved model = <<dataset_name>>_<<n_steps_val>>_<<n_shared_val>>_<<n_ind_val>>-<<n_a_val>>_<<n_d_val>>_<<eps_val>>_<<n_attacks_val>>.pt
             echo "Training model with n_steps = $n_steps, n_shared = $n_shared and n_ind = $n_ind :"
-            python3 train.py --dataset ieeecis --model tabnet_ieeecis --model_path $PATH_TO_MODELS/ieeecis_${n_steps}_${n_shared}_${n_ind}_16_16_${EPS_VAL}_${ATTACK_ITERS}.pt --n_steps $n_steps --n_shared $n_shared --n_ind $n_ind --delay ${DELAY_VAL} --attack_iters ${ATTACK_ITERS} --epochs 1000
+            python3 train.py --dataset ieeecis --model tabnet_ieeecis --model_path $PATH_TO_MODELS/ieeecis_${n_steps}_${n_shared}_${n_ind}_16_16_${EPS_VAL}_${ATTACK_ITERS}.pt --n_steps $n_steps --n_shared $n_shared --n_ind $n_ind --delay ${DELAY_VAL} --delay_threshold ${DELAY_THRESHOLD} --attack_iters ${ATTACK_ITERS} --epochs 1000
         done
     done
 done
--- parse_and_write_eval_out_csv.py
+++ parse_and_write_eval_out_csv.py
@@ -1,28 +1,43 @@
 import re
+import argparse
 
-# Opens the output file
-oup = open("ieeecis_adv_eval_out2.csv", mode='w')
 
-# Write the first row of the file
-oup.write("model[n_steps|n_shared|n_ind|eps_val|n_attack_iters],accuracy,cost-restricted,average-attack-cost,success_rate,rob_accuracy\n")
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_file", default="", type=str)
+    parser.add_argument("--output_file", default="", type=str)
+    return parser.parse_args()
 
-# Filters everything that isn't a number/a decimal
-regex = re.compile('[^0-9.|]')
+def main():
+    # Get the command-line arguments
+    args = get_args()
 
-# Opens the input file, performs a filtering and writes the filtered content to a CSV-file
-with open("ieeecis_adv_eval_2.txt", 'r') as inp:
-    # Skip the first two header lines
-    next(inp)
-    next(inp)
-    for line in inp:
-        if "../" in line:
-            continue
-        elif "Rob Acc" in line:
-            oup.write(regex.sub('', line))
-        elif "---" in line:
-            oup.write('\n')
-        else:
-            oup.write(regex.sub('', line)+',')
+    # Opens the output file
+    oup = open(args.output_file, mode='w')
 
-    inp.close()
-oup.close()
+    # Write the first row of the file
+    oup.write("model[n_steps|n_shared|n_ind|eps_val|n_attack_iters],accuracy,cost-restricted,average-attack-cost,success_rate,rob_accuracy\n")
+
+    # Filters everything that isn't a number/a decimal
+    regex = re.compile('[^0-9.|]')
+
+    # Opens the input file, performs a filtering and writes the filtered content to a CSV-file
+    with open(args.input_file, 'r') as inp:
+        # Skip the first two header lines
+        next(inp)
+        next(inp)
+        for line in inp:
+            if "../" in line:
+                continue
+            elif "Rob Acc" in line:
+                oup.write(regex.sub('', line))
+            elif "---" in line:
+                oup.write('\n')
+            else:
+                oup.write(regex.sub('', line)+',')
+
+        inp.close()
+    oup.close()
+
+if __name__ == "__main__":
+    main()
