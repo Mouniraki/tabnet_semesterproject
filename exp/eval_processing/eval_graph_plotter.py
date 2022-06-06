f8a0581da8642937455018da4b3d57182cab0e8e
Mounir Raki <mouniraki@protonmail.com>
2022-06-06 18:32:46 +0200

    Add word-wrap for graph titles

--- exp/eval_processing/eval_graph_plotter.py
+++ exp/eval_processing/eval_graph_plotter.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from textwrap import wrap
 
 
 def get_args():
@@ -82,23 +83,33 @@ def main():
 
     if args.plot_type == 'heat':
         mod_df = df[["accuracy","cost-restricted","average-attack-cost","success_rate","rob_accuracy"]]
-        s = sns.heatmap(mod_df, yticklabels=df['model_params'], annot=True)
+        s = sns.heatmap(mod_df, yticklabels=df['model_params'])
+
+        base_title = f"Heatmap of eval criterions for some TabNet config., with eps={df['eps_val'].iloc[0]}, n_attack_iters={df['n_attack_iters'].iloc[0]}"
+        text_arr = np.array([n_steps_str, n_shared_str, n_ind_str])
+        text_arr = np.array2string(text_arr[text_arr != ''], separator=', ').replace('[', '').replace(']', '')
+        if len(text_arr) > 0:
+            s.set_title("\n".join(wrap(f"{base_title} and conditioned on {text_arr}", 80)))
+        else:
+            s.set_title("\n".join(wrap(base_title, 80)))
+
         s.set_ylabel("Model config. [n_steps, n_shared, n_ind]")
         plt.show()
 
     else:
         # Perform projection on requested columns
-        df = df[np.append(['model_params'], args.plot_for)]
+        proj_df = df[np.append(['model_params'], args.plot_for)]
 
         # Plotting the graph (bar-kind for the moment)
-        df.plot(x = 'model_params', y = args.plot_for, kind = "bar")
+        proj_df.plot(x = 'model_params', y = args.plot_for, kind = "bar")
 
+        base_title = f"Bar plot of {args.plot_for} for some TabNet config., with eps={df['eps_val'].iloc[0]}, n_attack_iters={df['n_attack_iters'].iloc[0]}"
         text_arr = np.array([n_steps_str, n_shared_str, n_ind_str])
         text_arr = np.array2string(text_arr[text_arr != ''], separator=', ').replace('[', '').replace(']', '')
         if len(text_arr) > 0:
-            plt.title(f"Plotting {args.plot_for} for each kind of model, conditioned on {text_arr}")
+            plt.title("\n".join(wrap(f"{base_title}, keeping results only for models with {text_arr}", 80)))
         else:
-            plt.title(f"Plotting {args.plot_for} for each kind of model")
+            plt.title("\n".join(wrap(base_title, 80)))
 
         plt.xlabel("Model configuration [n_steps, n_shared, n_ind]")
         plt.show()
